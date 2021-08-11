6520041adb9bb35acf6054cc025789786a7c66ca
musanabiyev <musa416n@gmail.com>
2021-08-11 17:43:27 +0400

    uploading-files-initial

---
+++ src/main/java/com/company/UploadingFilesInitialApplication.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import com.company.storage.StorageProperties;
+import com.company.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesInitialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFilesInitialApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
